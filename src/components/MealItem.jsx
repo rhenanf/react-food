beb3f93988bf252f349ae054bc543c82a88e7f82
rhfolco <rhenan.folco@argenti.com.au>
2024-02-19 15:05:26 +1100

    implement reusable button component

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,4 +1,5 @@
 import { currencyFormatter } from '../util/formatting';
+import Button from './UI/Button.jsx';
 
 export default function MealItem({ meal }) {
 	return (
@@ -10,7 +11,7 @@ export default function MealItem({ meal }) {
 					<p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
 					<p className="meal-item-description">{meal.description}</p>
 					<p className="meal-item-actions">
-						<button>Add to Cart</button>
+						<Button>Add to Cart</Button>
 					</p>
 				</div>
 			</article>
