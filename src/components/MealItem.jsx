40484cbe031f334b2f896a2894ecaeb97ed87c3b
rhfolco <rhenan.folco@argenti.com.au>
2024-02-20 11:39:30 +1100

    implement a cart context

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,7 +1,16 @@
+import { useContext } from 'react';
+
 import { currencyFormatter } from '../util/formatting';
 import Button from './UI/Button.jsx';
+import CartContext from '../store/CartContext.jsx';
 
 export default function MealItem({ meal }) {
+	const cartCtx = useContext(CartContext);
+
+	function handleAddMealToCart() {
+		cartCtx.addItem(meal);
+	}
+
 	return (
 		<li className="meal-item">
 			<article>
@@ -11,7 +20,7 @@ export default function MealItem({ meal }) {
 					<p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
 					<p className="meal-item-description">{meal.description}</p>
 					<p className="meal-item-actions">
-						<Button>Add to Cart</Button>
+						<Button onClick={handleAddMealToCart}>Add to Cart</Button>
 					</p>
 				</div>
 			</article>
