c0a6693214d06671aeff792c036fbb1c872580d4
rhfolco <rhenan.folco@argenti.com.au>
2024-02-20 13:45:01 +1100

    implement cart view as a modal

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -5,10 +5,15 @@ export default function Modal({ children, open, className = '' }) {
 	const dialog = useRef();
 
 	useEffect(() => {
+    const modal = dialog.current;
+
 		if (open) {
-			dialog.current.showModal();
+			modal.showModal();
 		}
+
+    return () => modal.close();
 	}, [open]);
+  
 	return createPortal(
 		<dialog ref={dialog} className={`modal ${className}`}>
 			{children}
